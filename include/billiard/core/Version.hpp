076a66d638305d09c09783e24a5e13a64fec0503
Rafly Rabbany Z.P <rafflyrabbany@gmail.com>
2025-11-09 22:07:36 +0700

    init: add base CMake project and empty module directories

---
+++ include/billiard/core/Version.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+namespace billiard::core {
+
+// Simple version API for skeleton verification
+const char *version();
+
+} // namespace billiard::core
---
+++ src/core/Version.cpp
@@ -0,0 +1,7 @@
+#include "billiard/core/Version.hpp"
+
+namespace billiard::core {
+
+const char *version() { return "0.0.0-skeleton"; }
+
+} // namespace billiard::core
