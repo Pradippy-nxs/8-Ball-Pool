d7d4d884bb2e5b737a6a2e69196fdcdde61b11dd
dipta <mpradipta.arya03@gmail.com>
2025-11-15 11:03:09 +0700

    Added bad code to render balls

--- main_app/ENTITIES/Game.cpp
+++ main_app/ENTITIES/Game.cpp
@@ -1,5 +1,6 @@
 #include "Game.h"
 #include <SFML/Graphics.hpp>
+#include <iostream>
 
 void Game::render_window(){
     // Create the main window
@@ -22,6 +23,27 @@ void Game::render_window(){
     sprite.setScale({scale_x,scale_y});
  
     // Ball rendering
+    std::vector<sf::Texture*> ball_textures;
+    std::vector<sf::Sprite*> ball_sprites;
+
+    for (int i = 0; i < 16; i++)
+    {   
+        // Getting asset file names
+        std::string filename = "asset/ball_" + std::to_string(i+1) + ".png";
+        // std::cout<<filename<<std::endl;
+
+        // Making and storing texture
+        sf::Texture* texture_ptr = new sf::Texture(filename);
+        ball_textures.push_back(texture_ptr);
+        // std::cout<<"TEXTURES LOADED"<<std::endl;
+
+        // Making and storing sprites
+        sf::Sprite* sprite_ptr = new sf::Sprite(*texture_ptr);
+        sprite_ptr->setScale({scale_x,scale_y});
+        ball_sprites.push_back(sprite_ptr);
+        // std::cout<<"SPRITES LOADED"<<std::endl;
+
+    }
     
 
     // Start the game loop
@@ -40,7 +62,15 @@ void Game::render_window(){
  
         // Draw the sprite
         window.draw(sprite);
- 
+        
+        // Drawing balls
+        float initial_pos_x = 60;
+        float initial_pos_y = 360; 
+        for (sf::Sprite* ball_sprite : ball_sprites){
+            ball_sprite->setPosition({initial_pos_x, initial_pos_y});
+            window.draw(*ball_sprite);
+            initial_pos_x += 50;
+        }
         // Update the window
         window.display();
     }
